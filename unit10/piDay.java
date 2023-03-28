28e0113945dc91df0f3b225cf2912fdbc7605757
MaxOlson64 <127899070+MaxOlson64@users.noreply.github.com>
2023-03-28 21:17:54 +0000

    Max

---
+++ unit10/piDay.java
@@ -0,0 +1,17 @@
+public class piDay{
+    public static void main(String[] args) {
+        System.out.println("pi = " + pi);
+    }
+    public static double piDay(int n, boolean subtract){
+        if(n >= 1000){
+            return 0;
+        }else{
+            if(subtract==true){
+                -return 4.0/n + piDay(n + 2, subtract);
+            }else{
+                return 4.0/n + piDay(n + 2, subtract);
+            }
+           
+        }
+    }
+}
